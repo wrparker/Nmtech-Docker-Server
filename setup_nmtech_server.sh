4c72bc03f7cc9ac483e78aa82796a09e68b1d75b
W. Ryan Parker <bazookia@gmail.com>
2019-11-29 17:22:38 -0600

    bug fixes

--- setup_nmtech_server.sh
+++ setup_nmtech_server.sh
@@ -5,5 +5,21 @@ curl -sS https://get.docker.com/ | sh
 apt update
 apt install docker-compose -y
 
+# Make directories
 mkdir /var/www/
+mkdir -p /var/www/nginx-proxy/conf.d
+mkdir -p /var/www/nginx-proxy/certs
+mkdir -p /var/www/nginx-proxy/html
+mkdir -p /var/www/nginx-proxy/vhost.d
+
+# copy sites
 cp -r ./sites/* /var/www
+
+# Required network that all served containers must be added to.
+docker network create nginx-proxy
+
+systemctl enable docker
+systemctl restart docker
+cd /var/www/nginx-proxy && docker-compose up -d
+
+docker container ls
